b60cb8a4864ffaebc58252371dca9f7bba8ecae6
Benmc1 <benmc2002@gmail.com>
2024-04-03 12:16:31 +0100

    sprint 1 start

---
+++ src/main/java/API.java
@@ -0,0 +1,16 @@
+import com.theokanning.openai.embedding.Embedding;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.util.List;
+
+public class API {
+    static OpenAiService service = null;
+    static String  getChatCompletion(String Token, String Model){
+        return null;
+    }
+
+    static List<Embedding> getEmbedding(){
+        return List.of(null);
+    }
+
+}
