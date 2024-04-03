df512d8327e37542e7a6cf32c15cbd8038accefc
Benmc1 <benmc2002@gmail.com>
2024-04-03 12:30:29 +0100

    sprint 1 Api

--- src/main/java/API.java
+++ src/main/java/API.java
@@ -1,16 +1,44 @@
+import com.theokanning.openai.completion.chat.ChatCompletionChoice;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionResult;
+import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.embedding.Embedding;
+import com.theokanning.openai.embedding.EmbeddingRequest;
+import com.theokanning.openai.embedding.EmbeddingResult;
 import com.theokanning.openai.service.OpenAiService;
 
 import java.util.List;
 
 public class API {
     static OpenAiService service = null;
-    static String  getChatCompletion(String Token, String Model){
-        return null;
+    static String  getChatCompletion(String Token, String Model, List<ChatMessage> Messages){
+        if(service ==  null) service = new OpenAiService(Token);
+
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
+                .model(Model)
+                .messages(Messages)
+                .maxTokens(50)
+                .build();
+
+        ChatCompletionResult resultsList = service.createChatCompletion(chatCompletionRequest);
+
+        //only ever 1 choice unless stated otherwise in the chatCompletionRequest
+        ChatCompletionChoice result = resultsList.getChoices().get(0);
+        return result.getMessage().getContent();
     }
 
-    static List<Embedding> getEmbedding(){
-        return List.of(null);
+    static List<Embedding> getEmbedding(String Token, String Model, List<String> Message){
+        if(service ==  null) service = new OpenAiService(Token);
+
+        EmbeddingRequest embeddingRequest = EmbeddingRequest.builder()
+                .model(Model)
+                .input(Message)
+                .build();
+
+        EmbeddingResult result = service.createEmbeddings(embeddingRequest);
+
+
+        return result.getData();
     }
 
 }
