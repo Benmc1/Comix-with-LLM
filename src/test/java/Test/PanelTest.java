a30407dbd1c52c426b306f4b2514a8ff1539b8eb
Zeina Khattab <zeinakhattab2003@gmail.com>
2024-04-29 07:47:24 +0100

    added another panel test

--- src/test/java/Test/PanelTest.java
+++ src/test/java/Test/PanelTest.java
@@ -10,9 +10,16 @@ class PanelTest {
     @Test
     void string() {
         String[] lines = {"Hello there", "How are you", "This is a test"};
-        Panel panel = new Panel("tom","jack", lines);
+        Panel panel = new Panel("tom","jack", lines, "hollywood");
         String xmlAns = "Panel{charLeft='tom', charRight='jack', lines=[Hello there, How are you, This is a test], poseLeft='greeting', poseRight='shaking hands', setting='hollywood'}";
 
         assertEquals(xmlAns,panel.toString());
+    }
+
+    @Test
+    void testSettingGeneration() {
+        String[] lines = {"Hello", "Goodbye"};
+        Panel panel = new Panel("Alice", "Bob", lines, "Manhattan project");
+        assertEquals("manhattan", panel.getSetting()); 
     }
 }
