0378c7617eedebcb28fd9d06249f0077090f0f30
Zeina Khattab <zeinakhattab2003@gmail.com>
2024-05-04 14:17:02 +0100

    more tests added for new methods in Lines class

--- src/test/java/Test/LinesTest.java
+++ src/test/java/Test/LinesTest.java
@@ -3,6 +3,8 @@ package Test;
 import Main.Lines;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinesTest {
@@ -12,5 +14,26 @@ class LinesTest {
         Lines l = new Lines("Towers of sorrow");
         assertEquals(10, l.getAntiLines().size());
         assertEquals(10, l.getProLines().size());
+    }
+
+    @Test
+    public void test_returnsListOfStringArrays() {
+        Lines lines = new Lines("Speed limit");
+        List<String[]> suggestions = lines.generateBatchedSuggestions("speed limit");
+
+        assertNotNull(suggestions);
+        assertEquals(10, suggestions.size());
+        for (String[] suggestion : suggestions) {
+            assertEquals(3, suggestion.length);
+        }
+    }
+
+    @Test
+    public void test_handlesInvalidInputPromptAndReturnsEmptyList() {
+        Lines lines = new Lines("topic");
+        List<String[]> suggestions = lines.generateBatchedSuggestions("invalid_topic");
+
+        assertNotNull(suggestions);
+        assertTrue(suggestions.isEmpty());
     }
 }
